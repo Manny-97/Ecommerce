bec8d0a920f28fb74deadb700c290ab839d99565
Muhammed <muhammed@sendme.ng>
2022-08-06 15:47:30 +0100

    feat: unit test Product model

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -1,7 +1,7 @@
-from unicodedata import name
+from unicodedata import category, name
 from django.test import TestCase
 from store.models import Category, Product
-
+from django.contrib.auth.models import User
 
 class TestCategoriesModel(TestCase):
 
@@ -19,8 +19,23 @@ class TestCategoriesModel(TestCase):
         """
         data = self.data1
         self.assertTrue(isinstance(data, Category))
+        self.assertEqual(str(data), 'django')
     
-    def test_category_model_entry(self):
+
+        
+
+
+class TestProductsModel(TestCase):
+
+    def setUp(self) -> None:
+        Category.objects.create(name='django', slug='django')
+        User.objects.create(username='Muhammed')
+        self.data1 = Product.objects.create(category_id=1, title='django-beginners', 
+                                            slug='django-beginners', price='20.00', image='django', created_by_id=1)
+        # return super().setUp()
+
+    def test_product_model_entry(self):
         """ """
         data = self.data1
-        self.assertEqual(str(data), 'django')
+        self.assertTrue(isinstance(data, Product))
+        self.assertEqual(str(data), 'django-beginners')
