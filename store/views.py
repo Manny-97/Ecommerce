b398e5464a07ff4e2bb7c6064b973ff416d39134
Muhammed <muhammed@sendme.ng>
2022-08-06 17:01:21 +0100

    feat: categories list

--- store/views.py
+++ store/views.py
@@ -8,3 +8,6 @@ def all_products(request):
     products = Product.objects.all()
 
     return render(request, 'store/home.html', {'product': products})
+
+def categories(request):
+    return {'categories': Category.objects.all()}
