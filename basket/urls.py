7ca4686b9812915583ec3abb3cb9204b4c937323
Muhammed <muhammed@sendme.ng>
2022-08-08 10:57:22 +0100

    feat: style summary.html

--- basket/urls.py
+++ basket/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'basket'
 urlpatterns = [
-    path('', views.basket_summary, name='basket_summary')
+    path('', views.basket_summary, name='basket_summary'),
 ]
