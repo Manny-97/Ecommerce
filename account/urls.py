876db978d56530a99daa6201ab114077c7b99787
Muhammed <muhammed@sendme.ng>
2022-08-09 17:03:14 +0100

    feat: delete url

--- account/urls.py
+++ account/urls.py
@@ -7,9 +7,10 @@ app_name = 'account'
 
 urlpatterns = [
     path('login/', auth_view.LoginView.as_view(template_name='account/registration/login.html', form_class=UserLoginForm), name='login'),
-    path('logout/', auth_view.LogoutView.as_view(next_page='account/login/'), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(next_page='account/logout/'), name='logout'),
     path('register/', views.account_registration, name='register'),
     path('activate/<slug:uidb64>/<slug:token>/', views.account_activate, name='activate'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('profile/edit/', views.edit_details, name='edit_details'),
+    path('profile/delete', views.delete_user, name='delete_user'),
 ]
