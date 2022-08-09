17f506c2f8528dbfba13e8758b76b60bfcff0d29
Muhammed <muhammed@sendme.ng>
2022-08-09 15:14:48 +0100

    feat: dashboard url

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,5 @@ app_name = 'account'
 urlpatterns = [
     path('register/', views.account_registration, name='register'),
     path('activate/<slug:uidb64>/<slug:token>/', views.account_activate, name='activate'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
