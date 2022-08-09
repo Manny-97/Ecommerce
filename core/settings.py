f8b5a155a50f8aafd3f9a3569585aaab432b73ef
Muhammed <muhammed@sendme.ng>
2022-08-09 13:47:07 +0100

    feat: password token timeout duration

--- core/settings.py
+++ core/settings.py
@@ -135,3 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'account.UserBase'
 LOGIN_REDIRECT_URL = '/account/dashboard'
 LOGIN_URL = '/account/login'
+
+PASSWORD_RESET_TIMEOUT_DAYS = 2
