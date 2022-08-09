50612feb4fe6349266cadfedc73f96f2e38c3e4a
Muhammed <muhammed@sendme.ng>
2022-08-09 16:29:08 +0100

    feat: logout url

--- account/urls.py
+++ account/urls.py
@@ -7,6 +7,7 @@ app_name = 'account'
 
 urlpatterns = [
     path('login/', auth_view.LoginView.as_view(template_name='account/registration/login.html', form_class=UserLoginForm), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(next_page='account/login/'), name='login'),
     path('register/', views.account_registration, name='register'),
     path('activate/<slug:uidb64>/<slug:token>/', views.account_activate, name='activate'),
     path('dashboard/', views.dashboard, name='dashboard'),
