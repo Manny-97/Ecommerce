47511c43f5e409fe0e599022018648af3de1f0bb
Muhammed <muhammed@sendme.ng>
2022-08-09 15:46:04 +0100

    feat: login url

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_view
 from . import views
 
 app_name = 'account'
 
 urlpatterns = [
+    path('login/', auth_view.LoginView.as_view(template_name='account/registration/login.html', form_class=UserLoginForm), name='login'),
     path('register/', views.account_registration, name='register'),
     path('activate/<slug:uidb64>/<slug:token>/', views.account_activate, name='activate'),
     path('dashboard/', views.dashboard, name='dashboard'),
