49f01cb3303411a715a6c34d99420b61f732979c
Muhammed <muhammed@sendme.ng>
2022-08-10 09:04:36 +0100

    feat: setup stripe.js & elements to use in checkout form

--- static/payment/index.js
+++ static/payment/index.js
@@ -23,4 +23,36 @@ card.on('change', function(event) {
         displayError.textContent = '';
         $('#card-erros').removeClass('alert alert-info');
     }
+});
+
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev){
+    ev.preventDefault();
+var custName = document.getElementById("custName").value;
+var custAdd = document.getElementById("custAdd").value;
+var custAdd2 = document.getElementById("custAdd2").value;
+var postCode = document.getElementById("postCode").value;
+    stripe.confirmCardPayment(clientsecret, {
+        payment_method: {
+            card: card,
+            billing_details: {
+                address: {
+                    line1: custAdd,
+                    line2: custAdd2
+                },
+                name: custName
+            },
+        }
+    }).then(function(result) {
+        if (result.error) {
+            console.log(result.error.message);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                console.log('payment processed')
+
+                window.location.replace("http://127.0.0.1:8000/payment/orderplaced/");
+            }
+        }
+    });
 })
