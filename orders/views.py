d4dc1e4d747b12c581e2c28162917152be38542b
Muhammed <muhammed@sendme.ng>
2022-08-10 10:32:59 +0100

    feat: setup payment confirmation and user orders

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from basket.basket import Basket
+from django.http.response import JsonResponse
+from .models import Orders
 # Create your views here.
+
+def add(request):
+    return
+
+def payment_confirmation(data):
+    Orders.objects.filter(order_key=data).update(billing_status=True)
+
+def user_orders(request):
+    user_id = request.user.id
+    orders = Orders.objects.filter(user_id=user_id).filter(billing_status=True)
+    return orders
