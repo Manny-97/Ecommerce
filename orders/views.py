669cb8d65c81de239ec1cbe7c18880a338e35b38
Muhammed <muhammed@sendme.ng>
2022-08-10 11:10:49 +0100

    feat: add orders in order

--- orders/views.py
+++ orders/views.py
@@ -1,21 +1,35 @@
 from django.shortcuts import render
 from basket.basket import Basket
 from django.http.response import JsonResponse
-from .models import Orders
+from .models import Order, OrderItem
 # Create your views here.
+
 
 def add(request):
     basket = Basket(request)
     if request.POST.get('action') == 'post':
         order_key = request.POST.get('order_key')
         user_id = request.user.id
-    response = JsonResponse({})
-    return response
+        baskettotal = basket.get_total_price()
+
+        # Check if order exists
+        if Order.objects.filter(order_key=order_key).exists():
+            pass
+        else:
+            order = Order.objects.create(user_id=user_id, full_name='name', address1='add1',
+                                address2='add2', total_paid=baskettotal, order_key=order_key)
+            order_id = order.pk
+
+            for item in basket:
+                OrderItem.objects.create(order_id=order_id, product=item['product'], price=item['price'], quantity=item['qty'])
+
+        response = JsonResponse({'success': 'Return something'})
+        return response
 
 def payment_confirmation(data):
-    Orders.objects.filter(order_key=data).update(billing_status=True)
+    Order.objects.filter(order_key=data).update(billing_status=True)
 
 def user_orders(request):
     user_id = request.user.id
-    orders = Orders.objects.filter(user_id=user_id).filter(billing_status=True)
+    orders = Order.objects.filter(user_id=user_id).filter(billing_status=True)
     return orders
