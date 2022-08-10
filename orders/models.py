eaa62049d7400c441b0d9f64eb813f43f8ca47d9
Muhammed <muhammed@sendme.ng>
2022-08-10 11:01:48 +0100

    feat: order models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,37 @@
+from statistics import quantiles
 from django.db import models
+from django.conf import settings
+from decimal import Decimal
+from store.models import Product
 
 # Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='order_user', on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=50)
+    address1 = models.CharField(max_length=200)
+    address2 = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    post_code = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    total_paid = models.DecimalField(max_digits=5, decimal_places=2)
+    order_key = models.CharField(max_length=100)
+    billing_status = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return str(self.created)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+    
+    def __str__(self):
+        return str(self.id)
--- orders/views.py
+++ orders/views.py
@@ -5,7 +5,12 @@ from .models import Orders
 # Create your views here.
 
 def add(request):
-    return
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        order_key = request.POST.get('order_key')
+        user_id = request.user.id
+    response = JsonResponse({})
+    return response
 
 def payment_confirmation(data):
     Orders.objects.filter(order_key=data).update(billing_status=True)
