b00e94ba7d7419adfcb89d2d7ad07c9a7a8e4957
Muhammed <muhammed@sendme.ng>
2022-08-10 07:22:20 +0100

    feat: reset password url

--- account/urls.py
+++ account/urls.py
@@ -1,19 +1,21 @@
 from django.urls import path
-from django.contrib.auth import views as auth_view
-from .forms import UserLoginForm, PwdResetForm
+from django.contrib.auth import views as auth_views
+from .forms import UserLoginForm, PwdResetForm, PwdResetFormConfirm
 from . import views
 from django.views.generic import TemplateView
 
 app_name = 'account'
 
 urlpatterns = [
-    path('login/', auth_view.LoginView.as_view(template_name='account/registration/login.html', form_class=UserLoginForm), name='login'),
-    path('logout/', auth_view.LogoutView.as_view(next_page='account/logout/'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='account/registration/login.html', form_class=UserLoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='account/logout/'), name='logout'),
     path('register/', views.account_registration, name='register'),
     path('activate/<slug:uidb64>/<slug:token>/', views.account_activate, name='activate'),
     #Reset password
-    path('password_reset/', auth_view.PasswordResetView.as_view(template_name='account/user/password_reset_form.html',
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='account/user/password_reset_form.html',
                                                  success_url='password_reset_email_confrim', form_class=PwdResetForm), name='pwdreset'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetView.as_view(template_name='account/user/password_reset_form.html', 
+                                                success_url='account/password_reset_complete/', form_class=PwdResetFormConfirm), name='password_reset_confirm'),
     #Dashboard
     path('dashboard/', views.dashboard, name='dashboard'),
     path('profile/edit/', views.edit_details, name='edit_details'),
