4df9327dcd4ee4598786cce8712fd2af3277a692
Muhammed <muhammed@sendme.ng>
2022-08-10 07:32:58 +0100

    feat: password reset status page

--- account/urls.py
+++ account/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
                                                  success_url='password_reset_email_confrim', form_class=PwdResetForm), name='pwdreset'),
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetView.as_view(template_name='account/user/password_reset_form.html', 
                                                 success_url='account/password_reset_complete/', form_class=PwdResetConfirmForm), name='password_reset_confirm'),
+    path('password_reset/password_reset_email_confirm/', TemplateView.as_view(template_name='account/user/reset_status.html'), name='password_reset_done'),
     #Dashboard
     path('dashboard/', views.dashboard, name='dashboard'),
     path('profile/edit/', views.edit_details, name='edit_details'),
