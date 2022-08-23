055c735882959f95d687e62679d84e5bc43b06a8
Muhammed <muhammed@sendme.ng>
2022-08-23 14:50:19 +0100

    feat: addresses url

--- account/views.py
+++ account/views.py
@@ -10,7 +10,7 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from orders.views import user_orders
 
 from .forms import RegistrationForm, UserEditForm
-from .models import Customer
+from .models import Customer, Address
 from .token import account_activation_token
 
 
@@ -80,3 +80,9 @@ def account_activate(request, uidb64, token):
         return redirect('account:dashboard')
     else:
         return render(request, 'account/registration/activation_invalid.html')
+
+# Address
+@login_required
+def view_address(request):
+    addresses = Address.objects.filter(customer=request.user)
+    return render(request, "account/dashboard/addresses.html", {"addresses": addresses})
